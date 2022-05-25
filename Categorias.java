bce06f3be5793a104930b3532c1c927109e53788
portalviews <106222450+portalviews@users.noreply.github.com>
2022-05-25 04:28:41 -0300

    Create Categorias.java
    una  clase que lee una lista de competidores separados por renglones, posee una funcion para determinar si estos son pesos pesados o livianos,

---
+++ Categorias.java
@@ -0,0 +1,55 @@
+package luchadorJapones.luchadoresyPeso;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Categorias {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		categorizar("E:\\workSpace22\\novenaSemana\\src\\luchadorJapones\\luchadoresyPeso\\Altura");
+	}
+	
+	public static void categorizar (String rutadeAcceso) throws FileNotFoundException {
+		String categorias = null;
+		  File archivo = new File (rutadeAcceso);
+		  FileReader fr = new FileReader(archivo);
+		  BufferedReader bfr = new BufferedReader(fr);
+		  String linea; 
+		 
+		  int peso=0;
+		  int altura=0;
+		  int numerodeLinea=0;
+		  boolean pesados;
+		  String espesado ="es pesado";
+		  String esliviano = "es liviano";
+		  
+		  try( Scanner reader = new Scanner (archivo)) {
+			  while ((linea = bfr.readLine())!=null) {
+				  peso = reader.nextInt();
+				  altura = (int) reader.nextDouble();
+				  numerodeLinea++;
+			  if(peso>=70) {
+				  pesados=true;
+				  categorias = ("luchador "+numerodeLinea+": "+espesado+"\n");
+			  }else {
+				  pesados=false;
+				  categorias = ("luchador "+numerodeLinea+": "+esliviano+"\n");
+			  }
+//			  categorias +=categorias;
+			  System.out.println(categorias);
+			  }
+		  }catch(IOException e) {
+			  e.printStackTrace();
+		  }finally {
+			  
+		  }
+		  
+	}  
+
+}
