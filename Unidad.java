55412ad4deeac40874329103c029cda2a8b6c194
ferdem92debsas <79490598+ferdem92debsas@users.noreply.github.com>
2022-06-05 20:37:09 -0300

    Add files via upload

--- Unidad.java
+++ Unidad.java
@@ -1,11 +1,19 @@
 package juegoDeEstrategia;
 
 public abstract class Unidad {
-int vida;
-int posicion;
-boolean vivo;
+public int vida ;
+public int posicion;
+public boolean vivo;
+public int salud;
 
-abstract public void atacar();
+public Unidad(int vida2, int posicion2, boolean vivo2,int salud) {
+	this.vida = vida2;
+	this.posicion = posicion2;
+	this.vivo = vivo2;
+	this.salud = salud;
+}
+
+abstract public void atacar(Unidad jugador2);
 abstract public void recibirDaño();
 abstract public boolean vivo();
 }
